6597bbed41f98e767d014c5da8122bf966c5434e
Carlos Rosão <carlos.rosao@siroop.ch>
2018-04-18 16:51:20 +0200

    Add deal-card and header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeHeaderComponent } from './home-header/home-header.component';
 import { HomeDealComponent } from './home-deal/home-deal.component';
+import { HeaderComponent } from './header/header.component';
+import { DealCardComponent } from './deal-card/deal-card.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeHeaderComponent,
-    HomeDealComponent
+    HomeDealComponent,
+    HeaderComponent,
+    DealCardComponent
   ],
   imports: [
     BrowserModule

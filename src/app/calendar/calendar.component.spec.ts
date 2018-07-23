8aa89087c778b78f33e4bcf3ac6a0b9279a759fc
Carlos Rosão <shemahmforash@gmail.com>
2018-07-23 12:04:07 +0100

    Add calendar to calendar step

--- src/app/calendar/calendar.component.spec.ts
+++ src/app/calendar/calendar.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { CalendarComponent } from './calendar.component';
 
@@ -8,9 +10,14 @@ describe('CalendarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CalendarComponent ]
+      declarations: [CalendarComponent],
+      imports: [
+        OwlDateTimeModule,
+        OwlNativeDateTimeModule,
+        BrowserAnimationsModule
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {

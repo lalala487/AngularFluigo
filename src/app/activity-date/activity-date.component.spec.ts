49eeed22d7a81ffe7e9898cf68ea7e2c740c9742
Bastiaan Harmsen <bastiaan@flugio.com>
2018-10-30 22:44:49 +0100

    Activity Date Component, HTML & Styling

---
+++ src/app/activity-date/activity-date.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ActivityDateComponent } from './activity-date.component';
+
+describe('ActivityDateComponent', () => {
+  let component: ActivityDateComponent;
+  let fixture: ComponentFixture<ActivityDateComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ActivityDateComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ActivityDateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/activity-date/activity-date.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-date',
+  templateUrl: './activity-date.component.html',
+  styleUrls: ['./activity-date.component.css']
+})
+export class ActivityDateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

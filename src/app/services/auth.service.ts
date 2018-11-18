290fff0844f0fa631e38cdc4210dec3446bfdb23
Carlos Rosão <shemahmforash@gmail.com>
2018-11-18 18:54:02 +0000

    Use logged user email in user data step

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,13 +2,18 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { fromPromise } from 'rxjs/observable/fromPromise';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { User } from '../models/user';
 
 @Injectable()
 export class AuthService {
   public redirectUrl: string;
   private user: Observable<any>;
 
-  constructor(private afAuth: AngularFireAuth) {
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore
+    ) {
     this.user = afAuth.authState;
   }
 
@@ -22,6 +27,10 @@ export class AuthService {
 
   getUserIdToken(): Observable<string> {
     return fromPromise(this.afAuth.auth.currentUser.getIdToken());
+  }
+
+  getLoggedUser(): Observable<User> {
+    return this.afs.doc<User>(`users/${this.afAuth.auth.currentUser.uid}`).valueChanges();
   }
 
 }
--- src/app/user-data/user-data.component.ts
+++ src/app/user-data/user-data.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { User } from '../models/user';
+import { FirestoreService } from '../services/firestore.service';
+import { AuthService } from '../services/auth.service';
+import { switchMap } from 'rxjs/operator/switchMap';
 
 @Component({
   selector: 'app-user-data',
@@ -9,9 +12,15 @@ import { User } from '../models/user';
 export class UserDataComponent implements OnInit {
   @Input() user: User;
 
-  constructor() { }
+  constructor(
+    protected db: FirestoreService,
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
+    this.authService.getLoggedUser().subscribe(innerUser => {
+      this.user.email = innerUser.email;
+    });
   }
 
 }

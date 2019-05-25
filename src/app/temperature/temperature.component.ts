6add96be2a5d85f27944635db15281977d4bc0cd
Bastiaan Harmsen <bastiaan@flugio.com>
2019-05-25 15:21:52 +0200

    Temperature Component Final Styling

--- src/app/temperature/temperature.component.ts
+++ src/app/temperature/temperature.component.ts
@@ -13,9 +13,16 @@ export class TemperatureComponent implements OnInit {
     scaleShowVerticalLines: false,
     tooltips: {
       displayColors: false,
+      backgroundColor: 'rgba(235, 235, 235, 1)',
+      bodyFontStyle: 'normal',
+      bodyFontColor: 'rgba(19, 78, 94, 1)',
+      bodyFontFamily: "'Roboto', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
+      bodyFontSize: 14,
+      xPadding: 14,
+      yPadding: 14,
       callbacks: {
         label: function(tooltipItem, data) {
-          return tooltipItem.yLabel;
+          return tooltipItem.yLabel + ' Grad';
         },
         title: function(tooltipItem, data) {
           return;
@@ -30,7 +37,7 @@ export class TemperatureComponent implements OnInit {
             },
             ticks: {
               display: false,
-              fontSize: 13,
+              fontSize: 14,
               fontColor: 'rgba(235, 235, 235, 0.5)'
             }
         }],
@@ -43,7 +50,7 @@ export class TemperatureComponent implements OnInit {
               display: false,
               stepSize: 10,
               suggestedMin: 0,
-              fontSize: 13,
+              fontSize: 14,
               fontColor: 'rgba(235, 235, 235, 0.5)'
             }
         }]
